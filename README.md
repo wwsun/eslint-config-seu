b16d7e6fe51ca57b362d3b0d3bf6e0bb0dbcc92d
wwsun <ww.sun@outlook.com>
2018-03-07 12:54:34 +0800

    feat: first version

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # eslint-config-seu
+
 eslint rules for react projects
+
+```bash
+npm install eslint eslint-config-seu -D
+```
+
+create `.eslintrc` in root
+
+```json
+{
+    "extends": [
+        "eslint-config-seu"
+    ]
+}
+```
---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+    extends: 'airbnb',
+    parser: 'babel-eslint',
+    parserOptions: {
+        "sourceType": 'module',
+    },
+    rules: {
+        "intent": [2, 2, { "SwitchCase": 1 }],
+        "require-yield": "off",
+        "camelcase": "off",
+        "react/no-did-mount-set-state": "off"
+    }
+}
