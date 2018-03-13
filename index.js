e88b59a6354d6c2f9547edbfdcb16e07ad887a4a
wwsun <ww.sun@outlook.com>
2018-03-13 14:24:13 +0800

    feat: add rules

--- index.js
+++ index.js
@@ -13,7 +13,20 @@ module.exports = {
         "indent": [2, 2, { "SwitchCase": 1 }],
         "require-yield": "off",
         "camelcase": "off",
+        "radix": "off",
+        "max-len": "off",
+        "arrow-parens": "off",
+        "comma-dangle": "off",
+        "class-method-use-this": "off",
+        "no-restricted-syntax": "off",
+        "no-underscore-dangle": "off",
+        "no-param-reassign": "off",
+        "no-empty-pattern": "off",
+        "object-curly-newline": "off",
+        "prefer-destructuring": "off",
         "react/jsx-filename-extension": [0],
-        "react/no-did-mount-set-state": "off"
+        "react/no-did-mount-set-state": "off",
+        "react/jsx-closing-tag-location": "off",
+        "react/jsx-closing-bracket-location": "off"
     }
 }
